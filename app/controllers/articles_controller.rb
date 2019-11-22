4806b6202ff71fece3fe276732bd350773170278
parthbpatel <parthtech3010@gmail.com>
2019-11-22 18:27:42 +0530

    pundit article app using pundit and devide authentication

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,66 @@
+class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @articles = Article.all
+    authorize @articles
+  end
+
+  def show
+  end
+
+  def new
+    @article = Article.new
+    authorize @article
+  end
+
+  def edit
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.user = current_user
+    authorize @article
+
+    respond_to do |format|
+      if @article.save
+        format.html { redirect_to articles_path, notice: 'Article was successfully created.' }
+        format.json { render :show, status: :created, location: @article }
+      else
+        format.html { render :new }
+        format.json { render json: @article.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @article.update(article_params)
+        format.html { redirect_to @article, notice: 'Article was successfully updated.' }
+        format.json { render :show, status: :ok, location: @article }
+      else
+        format.html { render :edit }
+        format.json { render json: @article.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @article.destroy
+    respond_to do |format|
+      format.html { redirect_to articles_url, notice: 'Article was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+    def set_article
+      @article = Article.find(params[:id])
+      authorize @article
+    end
+
+    def article_params
+      params.require(:article).permit(:title, :body, :user_id)
+    end
+end
---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  resources :articles
+  devise_for :users
+
+  root to: "articles#index"
+end
